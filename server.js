9e3e262e2d6c847ae35397bf331e48e01779af07
Vova <bordiian.v@gmail.com>
2023-02-15 16:38:47 +0200

    connected built-in middleware

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.get('/', (req, res) => {
+// Built-in middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('^/$|/index(.html)?', (req, res) => {
   res.send('Hi')
+})
+
+app.get('/*', (req, res) => {
+  res.status(404).send('Not Found')
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
