861973cd3b603ff1bdd387784a44a5ace6279abd
Vova <bordiian.v@gmail.com>
2023-02-16 23:00:49 +0200

    created middleware for verifying accessToken for future protected routes

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+// const verifyToken = require('../middleware/verifyToken')
 
 router.get('^/$|/index(.html)?', (req, res) => {
     res.send('Hi')
   })
+// router.route('^/$|/index(.html)?')
+//   .get(verifyToken, (req, res) => {
+//          res.send('Hi')
+//       })
+  
 
 module.exports = router
 
