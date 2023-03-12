674f5d14a92d4cfeb6fad5f0291cd9199c425b9e
Vova <bordiian.v@gmail.com>
2023-03-12 13:58:17 +0200

    commit before ESLint refactoring

--- dist/config/connectDatabase.js
+++ dist/config/connectDatabase.js
@@ -1,17 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.set('strictQuery', false);
-const uri = process.env.DATABASE_URI;
+'use strict'
+const __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : { default: mod }
+}
+Object.defineProperty(exports, '__esModule', { value: true })
+const mongoose_1 = __importDefault(require('mongoose'))
+mongoose_1.default.set('strictQuery', false)
+const uri = process.env.DATABASE_URI
 const connectDatabase = async () => {
-    try {
-        await mongoose_1.default.connect(uri);
-    }
-    catch (error) {
-        console.log(error);
-    }
-};
-exports.default = connectDatabase;
+  try {
+    await mongoose_1.default.connect(uri)
+  } catch (error) {
+    console.log(error)
+  }
+}
+exports.default = connectDatabase
