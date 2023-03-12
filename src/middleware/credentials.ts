107b8ede8ff7883ac36e75da2f7933997827c20f
Vova <bordiian.v@gmail.com>
2023-03-12 14:27:14 +0200

    credentials eslint problem solved

--- src/middleware/credentials.ts
+++ src/middleware/credentials.ts
@@ -1,7 +1,7 @@
 import allowedHosts from '../config/allowedHosts'
-import { Request, Response, NextFunction } from 'express'
+import { type RequestHandler } from 'express'
 
-const credentials = (req: Request, res: Response, next: NextFunction) => {
+const credentials: RequestHandler = (req, res, next): void => {
   const origin = req.header('Origin')
   if (allowedHosts.includes(origin as string)) {
     res.set('Access-Control-Allow-Credentials', 'true')
