71384e7bebd41e38ee1cbdc73d2a33bd330b1cf7
Vova <bordiian.v@gmail.com>
2023-03-12 14:11:20 +0200

    logoutController eslint problems solved

--- dist/controllers/authControllers/logoutController.js
+++ dist/controllers/authControllers/logoutController.js
@@ -5,14 +5,14 @@ const User_1 = require("../../models/User");
 // Can't delete access token from there, DONT FORGET WHEN STARTING build frontend
 const handleLogout = async (req, res) => {
     const cookies = req.cookies;
-    if (!cookies?.jwt)
+    if (cookies?.jwt === null)
         return res.sendStatus(204); // No content
     const refreshToken = cookies.jwt;
     // Check database for refresh token
     const foundUser = await User_1.User.findOne({ refreshToken }).exec();
-    if (!foundUser) {
+    if (foundUser == null) {
         res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
-        return res.sendStatus(204); // No content 
+        return res.sendStatus(204); // No content
     }
     // Delete refreshToken in db
     foundUser.refreshToken = '';
--- src/controllers/authControllers/logoutController.ts
+++ src/controllers/authControllers/logoutController.ts
@@ -1,22 +1,21 @@
 import { User } from '../../models/User'
-import { Request, Response } from 'express'
+import { type Request, type Response } from 'express'
 
 interface Cookie {
   jwt: string
 }
 
-
 // Can't delete access token from there, DONT FORGET WHEN STARTING build frontend
-export const handleLogout = async (req: Request, res: Response) => {
+export const handleLogout = async (req: Request, res: Response): Promise<any> => {
   const cookies: Cookie = req.cookies
-  if (!cookies?.jwt) return res.sendStatus(204) // No content
+  if (cookies?.jwt === null) return res.sendStatus(204) // No content
   const refreshToken = cookies.jwt
 
   // Check database for refresh token
   const foundUser = await User.findOne({ refreshToken }).exec()
-  if (!foundUser) {
+  if (foundUser == null) {
     res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
-    return res.sendStatus(204) // No content 
+    return res.sendStatus(204) // No content
   }
 
   // Delete refreshToken in db
