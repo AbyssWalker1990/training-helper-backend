800c2fc44983baf481925bed35117d686b85d5d3
Vova <bordiian.v@gmail.com>
2023-03-12 10:52:08 +0200

    transition to typescript - done!

--- dist/middleware/verifyToken.js
+++ dist/middleware/verifyToken.js
@@ -1,20 +1,30 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 // Needed for future routes only for authorized users
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization
-  if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401)
-  const token = authHeader.split(' ')[1]
-  jwt.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET,
-    (err, decoded) => {
-      if (err) return res.sendStatus(403)
-      req.user = decoded.username
-      next()
+    const accessSecret = process.env.ACCESS_TOKEN_SECRET;
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader?.startsWith('Bearer '))
+        return res.sendStatus(401);
+    const token = authHeader.split(' ')[1];
+    const currentUser = req.name;
+    const decoded = jsonwebtoken_1.default.verify(token, accessSecret);
+    if (decoded === req.name) { // Need to add logic for comparing with database user
+        next();
     }
-  )
-}
-
-module.exports = verifyToken
+};
+exports.default = verifyToken;
+// OLD VESION
+// jwt.verify(
+//   token,
+//   accessSecret,
+//   (err, decoded) => {
+//     if (err) return res.sendStatus(403)
+//     req.user = decoded.username
+//     next()
+//   }
+// )
