97721ce9791cbfbf54f64fc293ba13bfc7d66ed0
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-06-24 22:14:53 +0300

    changed allowed hosts

--- dist/config/allowedHosts.js
+++ dist/config/allowedHosts.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const allowedHosts = [
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://training-helper-247e77a6b6b1.herokuapp.com/',
+    'http://training-helper-247e77a6b6b1.herokuapp.com/',
+    'http://training-helper-*.herokuapp.com/',
     'https://www.google.com',
     'https://training-helper-247e77a6b6b1.herokuapp.com'
 ];
--- src/config/allowedHosts.ts
+++ src/config/allowedHosts.ts
@@ -1,6 +1,9 @@
 const allowedHosts: string[] = [
   'http://localhost:3500',
   'http://localhost:3000',
+  'https://training-helper-247e77a6b6b1.herokuapp.com/',
+  'http://training-helper-247e77a6b6b1.herokuapp.com/',
+  'http://training-helper-*.herokuapp.com/',
   'https://www.google.com',
   'https://training-helper-247e77a6b6b1.herokuapp.com'
 ]
